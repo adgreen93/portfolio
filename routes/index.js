0d0a863aa2aff9d44455048fac51c48ba2b08825
Alexander Green <adgreen93@gmail.com>
2017-03-14 19:10:20 -0400

    added stripe charge function

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/charge', function(req, res, next){
+  var stripe = require("stripe")("sk_test_0B6jqeCbWYS774x1h9hDVmuV");
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+var token = req.body.stripeToken; // Using Express
+
+// Charge the user's card:
+var charge = stripe.charges.create({
+  amount: 1000,
+  currency: "usd",
+  description: "Example charge",
+  source: token,
+}, function(err, charge) {
+  // asynchronously called
+  res.send('success')
+});
+})
+
 module.exports = router;

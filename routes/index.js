4753a3b1bff55c4e2b9a71bd1baf3884f22bdedd
Alexander Green <adgreen93@gmail.com>
2017-03-15 21:21:06 -0400

    lil changes to the portfolio link and stripe fixes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.post('/charge', function(req, res, next){
@@ -12,10 +12,13 @@ router.post('/charge', function(req, res, next){
 // Token is created using Stripe.js or Checkout!
 // Get the payment token submitted by the form:
 var token = req.body.stripeToken; // Using Express
+var chargeAmount = req.body.chargeAmount;
+// so 1000 is ten dollars. so we need to add zeroes to the amount
+console.log(chargeAmount);
 
 // Charge the user's card:
 var charge = stripe.charges.create({
-  amount: 1000,
+  amount: chargeAmount,
   currency: "usd",
   description: "Example charge",
   source: token,
@@ -23,6 +26,23 @@ var charge = stripe.charges.create({
   // asynchronously called
   res.send('success')
 });
-})
+});
+
+// this is the smart checkout that we'll be working on
+stripe.customers.create({
+  email: "paying.user@example.com",
+  source: token,
+}).then(function(customer) {
+  // YOUR CODE: Save the customer ID and other info in a database for later.
+  return stripe.charges.create({
+    amount: 1000,
+    currency: "usd",
+    description: "Example charge",
+    customer: customer.id,
+  });
+}).then(function(charge) {
+  // Use and save the charge info.
+    res.send('success')
+});
 
 module.exports = router;
